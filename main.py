b05d1e664e108cdd645eb09e1ee25798545dacb0
Victorjonah-prog <victorjonah8@gmail.com>
2025-09-15 11:50:12 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from methodes import Array
+
+fruits = Array()
+
+
+
+print(fruits.append(4))
+print(fruits.append(3))
+print(fruits.remove(3)) 
+print(fruits.insert(2,7)) 
+print(fruits.pop())
---
+++ methodes.py
@@ -0,0 +1,33 @@
+# custom methodes
+
+class Array:
+
+    data = []
+
+
+    def append(self,value):
+        self.data += [value]
+        return self.data
+
+
+    def remove(self, value):  
+        new_number = []
+        for number in self.data:
+            if number != value:
+                new_number += [number] 
+        self.data = new_number  
+        return self.data
+
+    def insert(self,index,value):
+        new_list1 = self.data[:index]
+        new_list2 = self.data[index:]
+        self.data = new_list1 + [value] + new_list2
+        return self.data
+
+    def pop(self):     
+        del self.data[-1]   
+        return self.data   
+                     
+        
+
+
